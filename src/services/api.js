8e34ab5613882cb6cf49068198c14db1fa20f59f
acp <carvalhohuzumak@gmail.com>
2023-12-18 21:52:26 -0300

    testando variáveis de ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import dotenv from 'dotenv';
-dotenv.config();
+const url = process.env.API_URL
 
 export const api = axios.create({
-    baseURL: process.env.API_URL
+    baseURL: url
 });
